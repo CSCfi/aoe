9890d32737f94df341ac48a30d680eb2635c43d7
Mikael Saipori <saipori@pm.me>
2024-07-17 16:30:05 +0300

    Script update for the scheduled Job of GitHub Actions

--- update_submodules.sh
+++ update_submodules.sh
@@ -3,10 +3,12 @@
 # Initialize and update submodules
 git submodule update --init --recursive
 
-# Pull the latest changes in each submodule
-git submodule foreach "branch=$(git symbolic-ref --short HEAD); git fetch origin $branch; git pull origin $branch"
+# Pull the latest changes in each submodule with rebase strategy
+git submodule foreach "branch=$(git symbolic-ref --short HEAD); git fetch origin $branch; git pull --rebase origin $branch"
 
 # Add the updated submodules to the parent repository
 git add .
 git commit -m "Submodules updated to the latest commits"
 git push origin main
+
+echo "Submodules updated and changes pushed to GitHub."
